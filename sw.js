f120be73f63b1971605aed918f357ee93421ae8e
Wokgui <149906755+Wokgui@users.noreply.github.com>
2026-08-14 16:24:12 +0200

    Forcer le cache PWA du nouveau mauve lumineux

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'radio-intelligente-pwa-v60';
+const CACHE = 'radio-intelligente-pwa-v61';
 const APP_SHELL = [
   '/',
   '/index.html',
@@ -12,7 +12,7 @@ const APP_SHELL = [
   ,'/cloud-backup.js?v=4'
   ,'/vendor/supabase/supabase.js?v=1'
   ,'/radio-v51.css?v=5'
-  ,'/radio-v51.js?v=7'
+  ,'/radio-v51.js?v=8'
   ,'/radio-v51-core.js?v=1'
 ];
 
