0463469efea1474089b36862985fd6ce765c7507
Wokgui <149906755+Wokgui@users.noreply.github.com>
2026-08-15 18:05:42 +0200

    Adapter l’icône Radio au masque Samsung Android

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'radio-intelligente-pwa-v77';
+const CACHE = 'radio-intelligente-pwa-v80';
 const APP_SHELL = [
   '/',
   '/index.html',
@@ -6,6 +6,7 @@ const APP_SHELL = [
   '/favicon-32.png',
   '/icon-192.png',
   '/icon-512.png',
+  '/icon-maskable-192.png',
   '/apple-touch-icon.png',
   '/cover-default.png',
   '/silence.wav'
